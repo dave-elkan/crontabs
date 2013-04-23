13811655328fec172771caa38534342541ed204b
Dave Elkan <dave@edave.net>
2013-04-22 22:46:32 -0700

    7 - Reduce the granuality of cron expressions to 60 seconds.

--- src/js/CrontabsRunner.js
+++ src/js/CrontabsRunner.js
@@ -71,7 +71,7 @@ define(["underscore", "brace", "TabCollection", "TabStorage", "ChromeTabManager"
         scheduleTab: function(cronTab, chromeTab) {
             cronTab.getCrons().each(function(cron) {
                 if (cron.getExpression() && cron.getExpression() != "") {
-                    var l = later(1, true);
+                    var l = later(60, true);
                     schedules.push(l);
                     var parser = (cron.getType() === "text") ? enParser : cronParser;
                     var schedule = parser().parse(cron.getExpression());
