dd6d6088ea90548d6270b756b58e210f870e76c1
Dave Elkan <dave@edave.net>
2013-10-07 07:25:05 -0700

    Initial angularjs investigation

---
+++ src/js/crontabs.js
@@ -0,0 +1,3 @@
+angular.module('crontabs', [
+    'ngStorage'
+]);
---
+++ src/js/tabs/tabs.js
@@ -0,0 +1,97 @@
+"use strict";
+
+function TabCtrl($scope) {
+
+    $scope.operations = [{
+        id: "show",
+        name: "Show"
+    }, {
+        id: "reload",
+        name: "Reload"
+    }, {
+        id: "showAndReload",
+        name: "Show & Reload"
+    }, {
+        id: "close",
+        name: "Close"
+    }];
+
+    $scope.types = [{
+        id: "cron",
+        name: "Cron"
+    }, {
+        id: "text",
+        name: "Text"
+    }];
+
+    $scope.tabs = [{
+        "url":"http://dailyjs.com",
+        "crons": [{
+            "type": "cron",
+            "operation": "show",
+            "expression": "45 9 * * MON-FRI"
+        }]
+    }];
+
+    $scope.addCron = function(tab) {
+        tab.crons.push({
+            type: "cron",
+            expression: "",
+            operation: $scope.operations[0].id
+        })
+    };
+
+    $scope.addTextExpression = function(tab) {
+        tab.crons.push({
+            type: "text",
+            expression: "",
+            operation: $scope.operations[0].id
+        })
+    };
+
+    $scope.removeCron = function(tab, cronToRemove) {
+        var crons = [];
+        var tabsCrons = tab.crons;
+        if (tabsCrons.length > 1) {
+            tabsCrons.forEach(function(cron) {
+                if (cron !== cronToRemove) {
+                    crons.push(cron);
+                }
+            });
+            tab.crons = crons;
+        }
+    };
+
+    $scope.onChangeOperation = function(cron, operation) {
+        cron.operation = operation.id;
+    };
+
+    $scope.removeTab = function(tabToRemove) {
+        if ($scope.tabs.length > 1) {
+            var tabs = [];
+            $scope.tabs.forEach(function(tab) {
+                if (tab !== tabToRemove) {
+                    tabs.push(tab);
+                }
+            });
+
+            $scope.tabs = tabs;
+        }
+    };
+
+    $scope.addTab = function() {
+        $scope.tabs.push({
+            url: "",
+            crons: [{
+                url: "",
+                type: "cron",
+                operation: "show",
+                expression: ""
+            }]
+        });
+    };
+
+    $scope.onSubmit = function() {
+        debugger
+    };
+}
