84d8b85316bef1907043bfaebb7b043f78930e6d
Dave Elkan <dave@edave.net>
2014-02-15 11:33:58 -0800

    Add basic tabs and routing support. Add time management tab.

--- src/js/crontabs.js
+++ src/js/crontabs.js
@@ -1,3 +1,25 @@
 "use strict";
 
-var crontabs = angular.module('crontabs', ['webStorageModule']);
+var crontabs = angular.module('crontabs', ['webStorageModule', 'ngRoute']);
+
+crontabs.controller("NavigationCtrl", function($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+        return viewLocation === $location.path();
+    };
+});
+
+crontabs.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/time-management', {
+                templateUrl: 'src/partials/time-management.html',
+                controller: 'TimeManagementController'
+            }).
+            when('/advanced', {
+                templateUrl: 'src/partials/advanced.html',
+                controller: 'EditorCtrl'
+            }).
+            otherwise({
+                redirectTo: '/time-management'
+            });
+    }]);
