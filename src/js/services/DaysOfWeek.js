6c0090fdc8b0b4235359be9f0c046c3295c7a68c
Dave Elkan <dave@edave.net>
2014-02-15 16:28:26 -0800

    Add numbers to daysofweek for parsing purposes.

--- src/js/services/DaysOfWeek.js
+++ src/js/services/DaysOfWeek.js
@@ -1,24 +1,31 @@
 angular.module("crontabs").factory("DaysOfWeek", function(i18nManager) {
     return [{
         id: "MON",
+        num: 2,
         name: i18nManager("dayMonday")
     }, {
         id: "TUE",
+        num: 3,
         name: i18nManager("dayTuesday")
     }, {
         id: "WED",
+        num: 4,
         name: i18nManager("dayWednesday")
     }, {
         id: "THUR",
+        num: 5,
         name: i18nManager("dayThursday")
     }, {
         id: "FRI",
+        num: 6,
         name: i18nManager("dayFriday")
     }, {
         id: "SAT",
+        num: 7,
         name: i18nManager("daySaturday")
     }, {
         id: "SUN",
+        num: 0,
         name: i18nManager("daySunday")
     }];
 });
