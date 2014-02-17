7fef333894c12aaea307d81fab4d7308394beb61
Dave Elkan <dave@edave.net>
2014-02-17 11:42:05 -0800

    Fix background.js includes

--- karma.conf.js
+++ karma.conf.js
@@ -8,6 +8,8 @@ module.exports = function(config) {
             'lib/bower_components/jquery/jquery.min.js',
             'lib/bower_components/angular/angular.min.js',
             'lib/bower_components/angular-mocks/angular-mocks.js',
+            'lib/bower_components/angular-route/angular-route.js',
+            'lib/bower_components/checklist-model/checklist-model.js',
             'lib/bower_components/angular-webstorage/angular-webstorage.js',
             'test/lib/sinon-1.6.0.js',
             'js/crontabs.js',
