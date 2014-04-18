d02bf1a77e7b8967cc46bc7be21a02f218a6249c
Dave Elkan <dave@edave.net>
2014-04-17 21:29:57 -0700

    3 - Add Time Management tests.

--- test/TestTimeManagementService.js
+++ test/TestTimeManagementService.js
@@ -4,19 +4,49 @@ describe("TimeManagementService", function() {
 
     it("should find a tab with a show and close operation on the same days to be valid", inject(function(TimeManagementService) {
 
-        var singleDayOpenCloseTab = {
+        var tab = {
             crons: [{
                 operation: "show",
                 type: "cron",
-                expression: "0 30 12 * * MON *"
+                expression: "0 30 12 * * MON-FRI *"
             }, {
                 operation: "close",
                 type: "cron",
-                expression: "0 40 12 * * MON *"
+                expression: "0 40 12 * * MON-FRI *"
             }]
         };
 
-        expect(TimeManagementService.isCompatibleTab(singleDayOpenCloseTab)).toBeTruthy();
+        expect(TimeManagementService.isCompatibleTab(tab)).toBeTruthy();
+    }));
+
+    it("should find a tab with a show and close operation on the different days to be invalid", inject(function(TimeManagementService) {
+
+        var tab = {
+            crons: [{
+                operation: "show",
+                type: "cron",
+                expression: "0 30 12 * * MON-TUE *"
+            }, {
+                operation: "close",
+                type: "cron",
+                expression: "0 40 12 * * MON-THU *"
+            }]
+        };
+
+        expect(TimeManagementService.isCompatibleTab(tab)).toBeFalsy();
+    }));
+
+    it("should find a tab with only a close operation to be invalid", inject(function(TimeManagementService) {
+
+        var tab = {
+            crons: [{
+                operation: "close",
+                type: "cron",
+                expression: "0 40 12 * * MON-THU *"
+            }]
+        };
+
+        expect(TimeManagementService.isCompatibleTab(tab)).toBeFalsy();
     }));
 
 });
