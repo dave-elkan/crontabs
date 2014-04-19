656a4a4579bceec0679182ab2db6f7ec04431b20
Dave Elkan <dave@edave.net>
2014-04-19 16:15:13 -0700

    3 - Move logic from TimeManagementController into service.

--- app/scripts/controllers/TimeManagementController.js
+++ app/scripts/controllers/TimeManagementController.js
@@ -12,59 +12,9 @@ function($scope, Messaging, TabStorage, DaysOfWeek, i18nManager, TimeManagementS
 
     $scope.DaysOfWeek = DaysOfWeek;
 
-    var incompatibleTabs = [];
-    var compatibleTabs = [];
+    var tabs = TimeManagementService.getTabs($scope.tabs);
 
-    $scope.tabs.forEach(function(tab) {
-        if (TimeManagementService.isCompatibleTab(tab)) {
-            compatibleTabs.push(tab);
-        } else {
-            incompatibleTabs.push(tab);
-        }
-    });
-
-    if (!compatibleTabs.length) {
-        compatibleTabs = TabStorage.getEmptyTabList();
-    }
-
-    $scope.compatibleTabs = compatibleTabs;
-
-    $scope.compatibleTabs.forEach(function(tab) {
-        var cron = tab.crons[0];
-        if (!tab.days) {
-            tab.days = [];
-        }
-        DaysOfWeek.forEach(function(day) {
-            var result = TimeManagementService.getScheduleForExpression(cron);
-            if (result.schedules && result.schedules.length && result.schedules[0].d && _.contains(result.schedules[0].d, day.num)) {
-                tab.days.push(day.num);
-            }
-        });
-    });
-
-    $scope.compatibleTabs.forEach(function(tab) {
-        tab.crons.forEach(function(cron) {
-            var schedule = TimeManagementService.getScheduleForExpression(cron);
-            var hour = schedule.schedules[0].h;
-            var minute = schedule.schedules[0].m;
-
-            if (minute < 10) {
-                minute = "0" + minute;
-            }
-
-            if (hour < 10) {
-                hour = "0" + hour;
-            }
-
-            var time = [hour, minute].join(":");
-
-            if (cron.operation === "show" || cron.operation === "showAndReload") {
-                tab.open = time;
-            } else {
-                tab.close = time;
-            }
-        });
-    });
+    $scope.compatibleTabs = tabs.compatible;
 
     $scope.removeTab = function(tabToRemove) {
         if ($scope.compatibleTabs.length > 1) {
@@ -81,56 +31,12 @@ function($scope, Messaging, TabStorage, DaysOfWeek, i18nManager, TimeManagementS
     };
 
     $scope.addTab = function() {
-        $scope.compatibleTabs.unshift({
-            url: "",
-            crons: [{
-                type: "cron",
-                operation: "show",
-                expression: ""
-            }, {
-                type: "cron",
-                operation: "close",
-                expression: ""
-            }]
-        });
+        $scope.compatibleTabs.unshift(TimeManagementService.getNewTab());
     };
-
-    function getCronForTimeAndDays(time, days) {
-        var segments = time.split(":");
-        days = _.map(days, function(day) {
-            return _.find(DaysOfWeek, function(dayOfWeek) {
-                return dayOfWeek.num === day;
-            }).id
-        });
-        return [0, segments[1], segments[0], "*", "*", days.join(",")].join(" ");
-    }
-
-    function buildCron(time, days, operation) {
-        return {
-            type: "cron",
-            expression: getCronForTimeAndDays(time, days),
-            operation: operation
-        };
-    }
-
-    function buildTab(tab) {
-        return {
-            url: tab.url,
-            crons: [
-                buildCron(tab.open, tab.days, "show"),
-                buildCron(tab.close, tab.days, "close")
-            ]
-        }
-    }
-
-    function buildTabs() {
-        return incompatibleTabs.concat($scope.compatibleTabs.map(buildTab));
-    }
 
     $scope.onSubmit = function() {
-        TabStorage.setTabs(buildTabs());
-        Messaging.sendMessage("saved");
+        TimeManagementService.saveTabs($scope.compatibleTabs, tabs.incompatible);
         $scope.editor.$setPristine();
-    };
+    }
 
 }]);
---
+++ app/scripts/services/time-management/TimeManagementService.js
@@ -0,0 +1,118 @@
+angular.module("crontabs").factory("TimeManagementService", [
+
+        "TimeManagementCompatibilityService",
+        "TabStorage",
+        "DaysOfWeek",
+        "ScheduleService",
+
+        function(TimeManagementCompatibilityService, TabStorage, DaysOfWeek, ScheduleService) {
+
+            function getNewTab() {
+                return {
+                    url: "",
+                    crons: [{
+                        type: "cron",
+                        operation: "show",
+                        expression: ""
+                    }]
+                };
+            }
+
+            function populateTabDays(tab) {
+                var cron = tab.crons[0];
+                if (!tab.days) {
+                    tab.days = [];
+                }
+                DaysOfWeek.forEach(function(day) {
+                    var result = ScheduleService.getScheduleForExpression(cron);
+                    if (result.schedules && result.schedules.length && result.schedules[0].d && _.contains(result.schedules[0].d, day.num)) {
+                        tab.days.push(day.num);
+                    }
+                });
+            }
+
+            function populateTimes(tab) {
+                tab.crons.forEach(function(cron) {
+                    var schedule = ScheduleService.getScheduleForExpression(cron);
+                    var hour = schedule.schedules[0].h;
+                    var minute = schedule.schedules[0].m;
+
+                    if (minute < 10) {
+                        minute = "0" + minute;
+                    }
+
+                    if (hour < 10) {
+                        hour = "0" + hour;
+                    }
+
+                    var time = [hour, minute].join(":");
+
+                    if (cron.operation === "show" || cron.operation === "showAndReload") {
+                        tab.open = time;
+                    } else {
+                        tab.close = time;
+                    }
+                });
+            }
+
+            function populateTab(tab) {
+                populateTabDays(tab);
+                populateTimes(tab);
+
+                return tab;
+            }
+
+            function getCronForTimeAndDays(time, days) {
+                var segments = time.split(":");
+                days = _.map(days, function(day) {
+                    return _.find(DaysOfWeek, function(dayOfWeek) {
+                        return dayOfWeek.num === day;
+                    }).id
+                });
+                return [0, segments[1], segments[0], "*", "*", days.join(",")].join(" ");
+            }
+
+            function buildCron(time, days, operation) {
+                return {
+                    type: "cron",
+                    expression: getCronForTimeAndDays(time, days),
+                    operation: operation
+                };
+            }
+
+            function buildTab(tab) {
+                var crons = [buildCron(tab.open, tab.days, "show")];
+
+                if (tab.close) {
+                    crons.push(buildCron(tab.close, tab.days, "close"));
+                }
+
+                return {
+                    url: tab.url,
+                    crons: crons
+                }
+            }
+
+            function buildTabs(compatibleTabs, incompatibleTabs) {
+                return incompatibleTabs.concat(compatibleTabs.map(buildTab));
+            }
+
+            return {
+
+                getNewTab: getNewTab,
+
+                getTabs: function(tabs) {
+                    return {
+                        compatible: TimeManagementCompatibilityService.getCompatibleTabs(tabs).map(populateTab),
+                        incompatible: TimeManagementCompatibilityService.getIncompatibleTabs(tabs)
+                    };
+                },
+
+                saveTabs: function(compatibleTabs, incompatibleTabs) {
+                    TabStorage.setTabs(buildTabs(compatibleTabs, incompatibleTabs));
+                    Messaging.sendMessage("saved");
+                }
+
+            };
+        }]
+);
