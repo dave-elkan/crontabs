8bffe5b1c586b9739cbd557662d90dcb730825a8
Dave Elkan <dave@edave.net>
2014-05-14 21:52:43 -0700

    Move logic from controller to service.

--- app/scripts/controllers/EditorController.js
+++ app/scripts/controllers/EditorController.js
@@ -1,104 +1,57 @@
 angular.module("crontabs").controller("EditorCtrl", [
+
     '$scope',
     'Messaging',
     'TabStorage',
-    'TabOperations',
-    'TabTriggerTypes',
     'i18nManager',
     'LaterService',
+    'AdvancedScheduleService',
 
 function($scope,
          Messaging,
          TabStorage,
-         TabOperations,
-         TabTriggerTypes,
          i18nManager,
-         LaterService) {
+         LaterService,
+         AdvancedScheduleService) {
 
     LaterService.date.localTime();
 
     AbstractTabEditorController($scope, TabStorage, i18nManager);
 
-    $scope.operations = TabOperations;
-
-    $scope.types = TabTriggerTypes;
+    $scope.operations = AdvancedScheduleService.getOperations();
+    $scope.types = AdvancedScheduleService.getTabTriggerTypes();
 
     $scope.addCron = function(tab) {
-        tab.crons.push(getDefaultCronDefinition("cron"));
+        AdvancedScheduleService.addCronToTab(tab);
+        $scope.editor.$setDirty();
     };
 
     $scope.addTextExpression = function(tab) {
-        tab.crons.push(getDefaultCronDefinition("text"));
+        AdvancedScheduleService.addTextExpressionToTab(tab);
+        $scope.editor.$setDirty();
     };
 
     $scope.removeCron = function(tab, cronToRemove) {
-        var crons = [];
-        var tabsCrons = tab.crons;
-        if (tabsCrons.length > 1) {
-            tabsCrons.forEach(function(cron) {
-                if (cron !== cronToRemove) {
-                    crons.push(cron);
-                }
-            });
-            tab.crons = crons;
-            $scope.editor.$setDirty();
-        }
+        AdvancedScheduleService.removeCron(tab, cronToRemove);
+        $scope.editor.$setDirty();
     };
 
     $scope.removeTab = function(tabToRemove) {
-        if ($scope.tabs.length > 1) {
-            var tabs = [];
-            $scope.tabs.forEach(function(tab) {
-                if (tab !== tabToRemove) {
-                    tabs.push(tab);
-                }
-            });
-
-            $scope.tabs = tabs;
-            $scope.editor.$setDirty();
-        }
+        $scope.tabs = AdvancedScheduleService.removeTab($scope.tabs, tabToRemove);
+        $scope.editor.$setDirty();
     };
 
     $scope.addTab = function() {
-        $scope.tabs.unshift({
-            url: "",
-            crons: [getDefaultCronDefinition("cron")]
-        });
-    };
-
-    $scope.onChangeOperation = function(cron, operation) {
-        cron.operation = operation.id;
+        AdvancedScheduleService.addTab($scope.tabs);
+        $scope.editor.$setDirty();
     };
 
     $scope.onCronExpressionChanged = function(cron) {
-        if (cron.expression) {
-            var spaceCount = cron.expression.split(" ").length;
-            if (spaceCount >= 5 && spaceCount <= 6) {
-                var schedule = LaterService.parse.cron(cron.expression, true);
-                if (schedule.schedules && schedule.schedules.length && _.keys(schedule.schedules[0]).length) {
-                    var description = moment(LaterService.schedule(schedule).next(1)).calendar();
-
-                    cron.description = " - Next occurrence " + description;
-
-                    return;
-                }
-            }
-        }
-
-        cron.description = "";
+        AdvancedScheduleService.onCronExpressionChanged(cron);
     };
 
     $scope.onSubmit = function() {
-        TabStorage.setTabs(angular.copy($scope.tabs));
-        Messaging.sendMessage("saved");
+        AdvancedScheduleService.saveSchedule(angular.copy($scope.tabs));
         $scope.editor.$setPristine();
     };
-
-    function getDefaultCronDefinition(type) {
-        return {
-            type: type,
-            expression: "",
-            operation: "show"
-        }
-    }
 }]);
---
+++ app/scripts/services/advanced/AdvancedScheduleService.js
@@ -0,0 +1,85 @@
+angular.module("crontabs").factory("AdvancedScheduleService", [
+
+    "LaterService",
+    "TabStorage",
+    "Messaging",
+    "TabOperations",
+    "TabTriggerTypes",
+
+
+    function(LaterService,
+             TabStorage,
+             Messaging,
+             TabOperations,
+             TabTriggerTypes) {
+
+        function getDefaultCronDefinition(type) {
+            return {
+                type: type,
+                expression: "",
+                operation: "show"
+            }
+        }
+
+        return {
+
+            addCronToTab: function(tab) {
+                tab.crons.push(getDefaultCronDefinition("cron"));
+            },
+
+            addTextExpressionToTab: function(tab) {
+                tab.crons.push(getDefaultCronDefinition("text"));
+            },
+
+            removeCron: function(tab, cronToRemove) {
+                tab.crons = _.filter(tab.crons, function(cron) {
+                    return cron !== cronToRemove;
+                });
+            },
+
+            addTab: function(tabs) {
+                tabs.unshift({
+                    url: "",
+                    crons: [getDefaultCronDefinition("cron")]
+                });
+            },
+
+            removeTab: function(tabs, tabToRemove) {
+                return _.filter(tabs, function(tab) {
+                    return tab !== tabToRemove;
+                });
+            },
+
+            onCronExpressionChanged: function() {
+                if (cron.expression) {
+                    var spaceCount = cron.expression.split(" ").length;
+                    if (spaceCount >= 5 && spaceCount <= 6) {
+                        var schedule = LaterService.parse.cron(cron.expression, true);
+                        if (schedule.schedules && schedule.schedules.length && _.keys(schedule.schedules[0]).length) {
+                            var description = moment(LaterService.schedule(schedule).next(1)).calendar();
+
+                            cron.description = " - Next occurrence " + description;
+
+                            return;
+                        }
+                    }
+                }
+                cron.description = "";
+            },
+
+            saveSchedule: function(tabs) {
+                TabStorage.setTabs(tabs);
+                Messaging.sendMessage("saved");
+
+            },
+
+            getOperations: function() {
+                return TabOperations;
+            },
+
+            getTabTriggerTypes: function() {
+                return TabTriggerTypes;
+            }
+        }
+    }
+]);
