b6ea8695fb2a4bcc80394c512ab845062749a325
Dave Elkan <dave@easyagile.com>
2021-07-01 16:28:26 +1000

    Make the nav nice

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,30 +1,50 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Link, useLocation } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  nav: {
+    flexGrow: 1,
+    marginLeft: theme.spacing(3),
+  },
+}));
 
 const Nav = () => {
   const location = useLocation();
+  const classes = useStyles();
   return (
     <AppBar position="static">
-      <Tabs
-        value={location.pathname}
-        aria-label="simple tabs example"
-      >
-        <Tab
-          label="Time Management"
-          component={Link}
-          value="/"
-          to={'/'}
-        />
-        <Tab
-          label="Cron"
-          component={Link}
-          value="/cron"
-          to={'/cron'}
-        />
-      </Tabs>
+      <Toolbar>
+        <Typography variant="h6" noWrap>
+          Crontabs
+        </Typography>
+        <Tabs
+          value={location.pathname}
+          aria-label="simple tabs example"
+          className={classes.nav}
+        >
+          <Tab
+            label="Time Management"
+            component={Link}
+            value="/"
+            to={'/'}
+          />
+          <Tab
+            label="Cron"
+            component={Link}
+            value="/cron"
+            to={'/cron'}
+          />
+        </Tabs>
+      </Toolbar>
     </AppBar>
   );
 };
