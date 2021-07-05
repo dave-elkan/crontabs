ff2b35b63e96f014fe40745df920b2698371fb01
Dave Elkan <dave@easyagile.com>
2021-07-05 16:44:49 +1000

    Wire up creating new schedules

--- src/pages/advanced/AddScheduleButtons.tsx
+++ src/pages/advanced/AddScheduleButtons.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import { Tab } from '../../types';
+import { ScheduleType, Tab } from '../../types';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import { addSchedule } from '../../store/scheduleSlice';
+import { useDispatch } from 'react-redux';
 
 type PropsType = {
   tab: Tab;
@@ -16,13 +18,31 @@ const useStyles = makeStyles((theme) => ({
 
 const AddScheduleButtons = ({ tab }: PropsType) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  function addNewSchedule(type: ScheduleType) {
+    return () =>
+      dispatch(
+        addSchedule({
+          tabId: tab.id,
+          type,
+        }),
+      );
+  }
 
   return (
     <Box>
-      <Button className={classes.button} variant="outlined">
+      <Button
+        className={classes.button}
+        variant="outlined"
+        onClick={addNewSchedule('cron')}
+      >
         Add cron expression
       </Button>
-      <Button className={classes.button} variant="outlined">
+      <Button
+        className={classes.button}
+        variant="outlined"
+        onClick={addNewSchedule('text')}
+      >
         Add text expression
       </Button>
     </Box>
