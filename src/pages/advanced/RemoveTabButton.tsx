4a2f746bb80e6d6a7eba7cc5a1e18e668de3a74c
Dave Elkan <dave@easyagile.com>
2021-07-07 21:10:20 +1000

    Wire up removing tabs

--- src/pages/advanced/RemoveTabButton.tsx
+++ src/pages/advanced/RemoveTabButton.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Tab } from '../../types';
 import Button from '@material-ui/core/Button';
+import { useDispatch } from 'react-redux';
+import { removeTab } from '../../store/tabsSlice';
 
 type PropsType = {
   tab: Tab;
 };
 
 const RemoveTabButton = ({ tab }: PropsType) => {
+  const dispatch = useDispatch();
   return (
-    <Button variant="outlined" color="secondary">
+    <Button
+      variant="outlined"
+      color="secondary"
+      onClick={() => dispatch(removeTab(tab.id))}
+    >
       Remove tab
     </Button>
   );
